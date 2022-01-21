10bf96611b17cb368dbffb17c4d064ef69313fdb
Hector Perez Baranda <hector@esss.co>
2022-01-21 10:44:54 -0300

    :sparkles: Create models
    Creating models to store data information

--- eye/admin.py
+++ eye/admin.py
@@ -1,3 +1,11 @@
+from re import M
 from django.contrib import admin
+from .models import Event, DataEvent
 
-# Register your models here.
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("session", "category", "name", "timestamp")
+
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(DataEvent)
--- eye/models.py
+++ eye/models.py
@@ -1,3 +1,22 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class DataEvent(models.Model):
+    host = models.CharField(max_length=250)
+    path = models.CharField(max_length=250)
+    element = models.CharField(max_length=250, blank=True)
+
+    def __str__(self) -> str:
+        return self.host
+
+
+class Event(models.Model):
+    session = models.UUIDField(default=uuid.uuid4, editable=True)
+    category = models.CharField(max_length=50)
+    name = models.CharField(max_length=20)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    data = models.ForeignKey(DataEvent, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.name}-{self.category}"
