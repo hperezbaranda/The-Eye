27f6d53e83c68ef1e0078aebabca7b2cce0c0faf
Hector Perez Baranda <hector@esss.co>
2022-01-22 01:20:14 -0300

    :fire: Improve model representation

--- eye/models.py
+++ eye/models.py
@@ -1,30 +1,17 @@
 import uuid
 from django.db import models
-from django.forms import JSONField
-
-
-class DataEvent(models.Model):
-    """
-    Data payload for every event
-    """
-    
-    host = models.CharField(max_length=250)
-    path = models.CharField(max_length=250)
-    rest_data = models.JSONField(db_index=True, blank=True)
-
-    def __str__(self) -> str:
-        return self.host
 
 
 class Event(models.Model):
     """
     Event model to be used as base for analysis
     """
+
     session = models.UUIDField(default=uuid.uuid4, editable=True)
     category = models.CharField(max_length=50)
     name = models.CharField(max_length=20)
     timestamp = models.DateTimeField(auto_now_add=True)
-    data = models.ForeignKey(DataEvent, on_delete=models.CASCADE)
+    data = models.JSONField(default=dict, blank=True)
 
     def __str__(self) -> str:
         return f"{self.name}-{self.category}"
