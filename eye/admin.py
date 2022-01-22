dd09183c1b6f64ee7347ee4d6bcdb69cc79b1632
Hector Perez Baranda <hector@esss.co>
2022-01-22 15:19:06 -0300

    :hammer: Improving admin interface

--- eye/admin.py
+++ eye/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from .models import Event
-
+from django.contrib.admin.models import LogEntry
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ("session", "category", "name", "timestamp", "data")
+    list_display = ("session_id", "category", "name", "timestamp", "data")
+    def has_add_permission(self, request):
+        return False
+    def has_change_permission(self, request, obj=None):
+        return False
 
+    def has_view_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    search_fields = [
+       "session_id", "category", "name","timestamp"
+    ]
 
 admin.site.register(Event, EventAdmin)
