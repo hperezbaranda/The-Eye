14aa1480f66ea8c1ebf046ead8e829c3153c3b63
Hector Perez Baranda <hector@esss.co>
2022-01-22 15:16:11 -0300

    :hammer: Improving model data

--- eye/models.py
+++ eye/models.py
@@ -10,8 +10,8 @@ class Event(models.Model):
     session_id = models.UUIDField(default=uuid.uuid4, editable=True)
     category = models.CharField(max_length=50)
     name = models.CharField(max_length=20)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField()
     data = models.JSONField(default=dict, blank=True)
 
     def __str__(self) -> str:
-        return f"{self.name}-{self.category}"
+        return f"{self.name} {self.category}"
